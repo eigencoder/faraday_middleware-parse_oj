dc4afd9ee5019d4769ff45cdeb5eb8fc2867af2e
Pierre-Francoys Brousseau <pfbrousseau@truecar.com>
2016-06-30 14:14:29 -0700

    Change keys to be :snake_cased symbols. Will add flag or class to switch back and forth.

--- lib/faraday_middleware/parse_oj.rb
+++ lib/faraday_middleware/parse_oj.rb
@@ -3,12 +3,19 @@ require 'faraday_middleware/response_middleware'
 module FaradayMiddleware
   class ParseOj < ResponseMiddleware
     dependency 'oj'
-    
+
     define_parser do |body|
-      Oj.load(body, mode: :compat) unless body.strip.empty?
+      data = Oj.load(body, mode: :compat) unless body.strip.empty?
+      if data.respond_to?('deep_transform_keys!')
+        data.deep_transform_keys! { |key| key.underscore.to_sym }
+      else
+        data = {'root'=> data} # Wrap data to ensure deep_transform_keys works
+        data.deep_transform_keys! { |key| key.underscore.to_sym }
+        data = data[:root] # Back to original structure
+      end
     end
-    
-    VERSION = '0.3.0'
+
+    VERSION = '0.3.0.1'
   end
 end
 
